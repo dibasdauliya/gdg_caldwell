ed7be85427f0028637f20de9075a020d09cda22b
dibasdauliya <dibasdauliya@gmail.com>
2024-12-03 16:50:33 -0500

    edit apply link

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -33,7 +33,7 @@ export default function HeroSection() {
         </p>
         <div className="flex flex-col items-center space-y-6">
           <a
-            href="https://gdg.community.dev/gdg-on-campus-caldwell-university-caldwell-united-states/#c6dbSdWXGHW"
+            href="https://gdg.community.dev/gdg-on-campus-caldwell-university-caldwell-united-states/"
             rel="noopener noreferrer"
             target="_blank"
             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-700 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
